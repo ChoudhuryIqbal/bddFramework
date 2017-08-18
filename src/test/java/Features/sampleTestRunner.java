76dae410fdaaeda472355bd5e5c53286a295ac62
Iqbal <iqbalchoudhury2@gmail.com>
2017-08-18 08:53:25 -0500

    Reporting added

--- src/test/java/Features/sampleTestRunner.java
+++ src/test/java/Features/sampleTestRunner.java
@@ -7,13 +7,17 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 /**
  * @author choudhuryIqbal
  r*
  */
-@RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\java\\Features",glue= {"Steps"})
-public class sampleTestRunner {
+//@RunWith(Cucumber.class)
+@CucumberOptions(features="src\\test\\java\\Features",glue= {"Steps"}
+	,format = {"json: target/cucumber.json","html:target/size/cucumber-pretty"}
+		
+		)
+public class sampleTestRunner extends AbstractTestNGCucumberTests{
 
 }
